52305bb2ab876a551d5543ae05afa0d668a093a2
Farouk Bello <faroukk.bello@gmail.com>
2023-03-02 12:59:13 +0100

    Responsive UI Implemented...

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,15 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "6ab30259ef697ab9d4f01ad8e40ec95f",
-"/": "6ab30259ef697ab9d4f01ad8e40ec95f",
+  "index.html": "8e3b20b5b13c751d826ebbf558e50f7a",
+"/": "8e3b20b5b13c751d826ebbf558e50f7a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "version.json": "371812bd00996e2028f610beefedc90b",
-"main.dart.js": "c6f8e59b0d26dbe439f7f381dfdbfee1",
+"main.dart.js": "2f57995d5eb25a7278f9baf852a633d3",
 "manifest.json": "4f762937a306825eb0db6fadd9ec6a14",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/Poppins-Regular.ttf": "8b6af8e5e8324edfd77af8b3b35d7f9c",
